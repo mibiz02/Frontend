65df043b183c87c293969b425e088dcef6609ca5
unknown <director20844@gmail.com>
2022-11-16 23:37:29 +0900

    feat : 화살표에 맞게 타이틀도 색깔 바뀌는 것 적용, 색깔 바뀌는 부분 vuex로 넣음

--- src/components/MbtiCard.vue
+++ src/components/MbtiCard.vue
@@ -2,7 +2,7 @@
     <div class="mbti_card">
         <div class="img_box">
             <img :src="loadImg" :alt="type"/>
-            <div :style="setStyle"></div>
+            <div :style="getStyle"></div>
         </div>
         <div class="mbti_info">
             <h3>{{type}}</h3>
@@ -22,32 +22,16 @@
         },
         computed: {
             loadImg() {
-                return require(`../assets/mbti/${this.imgUrl}`)
+              return require(`../assets/mbti/${this.imgUrl}`)
             },
-            setStyle() {
-                let style = ''
-
-                switch (this.idx) {
-                    case 0:
-                        style = ` background-image: rgb(166,34,195);
-                                  background-image: linear-gradient(0deg, rgba(166,34,195,1) 0%, rgba(253,45,77,1) 100%);`
-                        break;
-                    case 1:
-                        style = ` background-image: rgb(34,193,195);
-                                  background-image: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);`
-                        break;
-                    case 2:
-                        style = ` background-image: rgb(34,195,161);
-                                  background-image: linear-gradient(0deg, rgba(34,195,161,1) 0%, rgba(45,115,253,1) 100%)`;
-                        break;
-                    case 3:
-                        style = ` background-image: rgb(232,119,52);
-                                  background-image: linear-gradient(0deg, rgba(232,119,52,1) 0%, rgba(253,191,45,1) 100%);`
-                        break;
-                }
-
-                return style
+            getStyle() {
+              return this.$store.state.mbti_style
             }
+        },
+        methods: {
+          check() {
+            console.log(this.style)
+          }
         }
     }
 </script>
--- src/layout/InputMBTI.vue
+++ src/layout/InputMBTI.vue
@@ -1,54 +1,76 @@
 <template>
-  <div id = "select_mbti" class="container">
-    <div id="__zone">
-        <div @click="subPage" class="select_arrow">
-            <img :src="previous" alt="previous"/>
+    <div id="select_mbti" class="container">
+        <div class="select_title">
+            <div class="go_link">
+                <a>MBTI 검사하러 가기</a>
+                <p>❗검사는 총 50문항으로 20분 가량 소요됩니다</p>
+            </div>
+            <div class="title">
+                <h1>보고싶은
+                    <p :style="getStyle">MBTI</p>를 선택하세요</h1>
+            </div>
         </div>
-        <div id="select_zone">
-            <MbtiCard v-for="item in mbti_lst[page].mbti" v-bind:key="item.name" :type="item.type" :text="item.text" :imgUrl="item.imgUrl" :idx="page"/>
-        </div>
-        <div @click="addPage" class="select_arrow">
-            <img :src="next" alt="next"/>
+        <div id="__zone">
+            <div @click="subPage" class="select_arrow">
+                <img :src="previous" alt="previous"/>
+            </div>
+            <div id="select_zone">
+                <MbtiCard
+                    v-for="item in mbti_lst[page].mbti"
+                    v-bind:key="item.name"
+                    :type="item.type"
+                    :text="item.text"
+                    :imgUrl="item.imgUrl"
+                    :idx="page"/>
+            </div>
+            <div @click="addPage" class="select_arrow">
+                <img :src="next" alt="next"/>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
-import MbtiCard from '../components/MbtiCard.vue';
-export default { 
-    name : 'inputMBTI',
-    components : {
-        MbtiCard
-    },
-    data() {
-        return {
-            mbti_lst : this.$store.state.type_mbti,
-            page : 0,
-            previous : require('../assets/previous.png'),
-            next : require('../assets/next.png'),
-        }
-    },
-    methods : {
-        addPage() {
-            let tmp = this.page+1;
-            if (tmp > 3) {
-                tmp = 0;
-            }
-            this.page = tmp
+    import MbtiCard from '../components/MbtiCard.vue';
+    export default {
+        name: 'inputMBTI',
+        components: {
+            MbtiCard
         },
-        subPage() {
-            let tmp = this.page-1;
-            if (tmp < 0) {
-                tmp = 3;
+        data() {
+            return {mbti_lst: this.$store.state.type_mbti, page: 0, previous: require('../assets/previous.png'), next: require('../assets/next.png'), style: this.$store.state.mbti_style}
+        },
+        computed: {
+            getStyle() {
+                return this.$store.state.mbti_style
             }
-            this.page = tmp
+        },
+        methods: {
+            addPage() {
+                let tmp = this.page + 1;
+                if (tmp > 3) {
+                    tmp = 0;
+                }
+                this.page = tmp
+                this
+                    .$store
+                    .commit('setStyle', this.page)
+            },
+            subPage() {
+                let tmp = this.page - 1;
+                if (tmp < 0) {
+                    tmp = 3;
+                }
+                this.page = tmp
+                this
+                    .$store
+                    .commit('setStyle', this.page)
+            }
         }
-    }
 
-}
+    }
 </script>
 
 <style>
-@import '../css/InputMBTI.css';
+    @import '../css/InputMBTI.css';
 </style>
