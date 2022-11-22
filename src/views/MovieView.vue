8d6fb9bbb916f52e1de00c2a047704de54319d1f
SSAFY <director20844@gmail.com>
2022-11-22 10:06:24 +0900

    feat : commentBox Movieview에 추가

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,27 +1,34 @@
 <template>
     <div>
-        <SignNavBar />
+        <SignNavBar/>
         <MovieDetail :movie="this.$store.state.movie"/>
         <CompatibilityList title="등장하는"/>
+        <div class="comment_body">
+            <CommentBox/>
+        </div>
     </div>
 </template>
 
 <script>
-  import MovieDetail from '../components/MovieDetail';
-  import SignNavBar from '../layout/SignNavBar';
-  import CompatibilityList from '../layout/CompatibilityList'
+    import MovieDetail from '../components/MovieDetail';
+    import SignNavBar from '../layout/SignNavBar';
+    import CompatibilityList from '../layout/CompatibilityList'
+    import CommentBox from '../layout/CommentBox.vue'
 
-  export default {
-      name: 'movieView',
-      components:{
-        MovieDetail,
-        SignNavBar,
-        CompatibilityList
-      },
-      created() {
-              this.$store.dispatch('GET_MOVIE_DATA', this.$route.query.id)
-      }
-  }
+    export default {
+        name: 'movieView',
+        components: {
+            MovieDetail,
+            SignNavBar,
+            CompatibilityList,
+            CommentBox
+        },
+        created() {
+            this
+                .$store
+                .dispatch('GET_MOVIE_DATA', this.$route.query.id)
+        }
+    }
 </script>
 
 <style></style>
